37260ffe24687b92a2178380f08b2ea4156b1d53
Wifi <oliwier.zar@wp.pl>
2025-07-16 07:35:14 +0200

    Blackjack added to menu

--- Casino/menu.py
+++ Casino/menu.py
@@ -1,4 +1,5 @@
 import diceroll
+import blackjack
 
 while True:
     wallet=input("How much do you want to deposit: ")
@@ -10,16 +11,19 @@ while True:
 while True:
     print("\n=== MENU ===")
     print("1. 🎲 Play Dice")
-    print("2. 💰 Show Balance")
-    print("3. 🚪 Exit Casino")
+    print("2. ♠️ Play Black Jack")
+    print("3. 💰 Show Balance")
+    print("4. 🚪 Exit Casino")
 
     choice = input("Choose option: ")
 
     if choice == "1":
         wallet=diceroll.dice_roll(wallet)
-    elif choice =="2":
+    elif choice=="2":
+        wallet=blackjack.black_jack(wallet)
+    elif choice =="3":
         print(f"\nYour balance is {wallet:.2f}$")
-    elif choice == "3":
+    elif choice == "4":
         print(f"Bye bye! Your payout is {wallet:.2f}$")
         break
     else:
