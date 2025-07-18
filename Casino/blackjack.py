0ab75b938c0fad0cc29def47d7160be489c1c1a1
Wifi <oliwier.zar@wp.pl>
2025-07-18 10:07:23 +0200

    Counting cards function

--- Casino/blackjack.py
+++ Casino/blackjack.py
@@ -1,8 +1,6 @@
 import random
 
 deck=[]
-player_cards=[]
-dealer_cards=[]
 
 card_values = {
     '2': 2, '3': 3, '4': 4, '5': 5, '6': 6,
@@ -25,13 +23,44 @@ def draw_card():
         create_deck()
     return deck.pop() if deck else None
 
+def calculate_hand_value(hand):
+    value=0
+    aces=0
+    for card in hand:
+        rank=card[:-1]
+        card_val=card_values.get(rank,0)
+        value+=card_val
+        if rank == 'A':
+            aces+=1
+    while value>21 and aces>0:
+        value-=10
+        aces-=1
+    return value
+
 def black_jack(wallet):
-    if not deck:
-        create_deck()
-    player_cards.clear()
-    dealer_cards.clear()
-    for draw in range (2):
-        player_cards.append(draw_card())
-        dealer_cards.append(draw_card())
-        draw+=1
+    while True:
+        player_cards=[]
+        dealer_cards=[]
+
+        bet_size=input("How much do you want to bet? (press q to exit blackjack):")
+        if bet_size=="q":
+            break
+        if not bet_size.isdigit():
+            print("You need to enter a numeric value")
+            continue
+        bet_size=float(bet_size)
+        if bet_size>wallet:
+            print(f"You have only {wallet}$")
+            continue
+        
+
+        if not deck:
+            create_deck()
+
+        for _ in range (2):
+            player_cards.append(draw_card())
+            dealer_cards.append(draw_card())
+
+        print(f"Dealer cards: [{dealer_cards[0]}, '?']")
+        print(f"Your cards: {player_cards}")
     return wallet
